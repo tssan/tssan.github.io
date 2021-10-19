cdb5b819d98452090deae2fbc11c2de90e3cf354
scarab <piotr.karasinski@gmail.com>
2021-10-19 14:17:42 +0200

    CV PDF generator

--- theme/js/blog.js
+++ theme/js/blog.js
@@ -9,24 +9,6 @@ async function registerSW() {
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    // const $nav = document.getElementById('main-nav');
-    // const $menuOpenBtn = document.getElementsByClassName('btn-menu-open')[0];
-    // const $menuCloseBtn = document.getElementsByClassName('btn-menu-close')[0];
-
-    // $menuOpenBtn.addEventListener('click', e => {
-    //     console.log('A');
-    //     $nav.classList.remove('desktop-hidden');
-    //     $menuCloseBtn.classList.remove('desktop-hidden');
-    //     $menuOpenBtn.classList.add('desktop-hidden');
-    // });
-
-    // $menuCloseBtn.addEventListener('click', e => {
-    //     console.log('B');
-    //     $menuOpenBtn.classList.remove('desktop-hidden');
-    //     $menuCloseBtn.classList.add('desktop-hidden');
-    //     $nav.classList.add('desktop-hidden');
-    // });
-
     // register service worker
     registerSW();
 });
